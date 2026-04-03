0a2ea1cd55d14104c64b82bde7c805194a819c95
dansut24 <Danieljamessutton18@outlook.com>
2026-04-03 14:32:02 +0100

    Update page.js

--- app/select-module/page.js
+++ app/select-module/page.js
@@ -1,5 +1,7 @@
 import AppShell from "@/components/app-shell"
+import { bootstrapSignedInUser } from "@/lib/bootstrap-user"
 
-export default function SelectModulePage() {
+export default async function SelectModulePage() {
+  await bootstrapSignedInUser()
   return <AppShell initialView="modules" />
 }
