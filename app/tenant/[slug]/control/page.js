b71b2e9576b92f8cbbad86f281ea2a9206952a77
dansut24 <Danieljamessutton18@outlook.com>
2026-04-04 07:04:36 +0100

    Create page.js

---
+++ app/tenant/[slug]/control/page.js
@@ -0,0 +1,9 @@
+import AppShell from "@/components/app-shell"
+import { requireTenantAccess } from "@/lib/tenant/require-tenant-access"
+
+export default async function TenantControlPage({ params }) {
+  const { slug } = await params
+  await requireTenantAccess(slug)
+
+  return <AppShell initialView="app" forcedModule="control" />
+}
