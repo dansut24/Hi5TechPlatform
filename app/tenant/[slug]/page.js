d1f9a5c60e22946fd634a8d027b4a3d6f57c0d7a
dansut24 <Danieljamessutton18@outlook.com>
2026-04-14 21:28:47 +0100

    Update page.js

--- app/tenant/[slug]/page.js
+++ app/tenant/[slug]/page.js
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation"
 
 export default async function TenantIndexPage({ params }) {
-  const { slug } = await params
+  const { slug } = params
   redirect(`/tenant/${slug}/dashboard`)
 }
